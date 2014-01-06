ea30e5a0bf855dc63e8b17af3eb71e6a8bcec746
Andreas Öhlund <andreas.ohlund@particular.net>
2014-01-06 15:28:04 +0100

    Making the test saga handle the message

---
+++ src/ServiceControl.UnitTests/AAA/SagaSpecification.cs
@@ -0,0 +1,110 @@
+﻿namespace ServiceControl.UnitTests.AAA
+{
+    using System;
+    using System.Collections.Generic;
+    using NServiceBus;
+    using NServiceBus.Features;
+    using NServiceBus.Persistence.InMemory.SagaPersister;
+    using NServiceBus.Pipeline;
+    using NServiceBus.Pipeline.Contexts;
+    using NServiceBus.Sagas;
+    using NServiceBus.Unicast;
+    using NServiceBus.Unicast.Behaviors;
+    using NServiceBus.Unicast.Messages;
+    using NUnit.Framework;
+    using Raven.Database.Linq.PrivateExtensions;
+
+    public class SagaSpecification<T>
+    {
+        public SagaSpecification()
+        {
+            Sagas.ConfigureSaga(typeof(T));
+        }
+
+        public void Given(object message)
+        {
+
+        }
+
+        public void When<TMessage>(TMessage message)
+        {
+            ApplyMessageToSaga(message);
+        }
+
+        void ApplyMessageToSaga(object message)
+        {
+            var metadata = new MessageMetadata {MessageType = message.GetType()};
+
+            var logicalMessage = new LogicalMessage(metadata, message, new Dictionary<string, string>());
+            var messageHandler = new MessageHandler
+            {
+                Instance = Activator.CreateInstance<T>(),
+            };
+
+            //todo removed when the core has been refactored
+            messageHandler.Invocation = (handlerInstance, m) => HandlerInvocationCache.InvokeHandle(handlerInstance, m);
+
+            context = new HandlerInvocationContext(new ReceiveLogicalMessageContext(new RootContext(builder), logicalMessage),messageHandler );
+
+            var sagaPersistenceBehavior = new SagaPersistenceBehavior
+            {
+                SagaPersister = new InMemorySagaPersister()
+            };
+
+            //todo: removed when core has been patched to not use Headers.GetMessageHeader
+            ExtensionMethods.GetHeaderAction = (m, key) =>
+            {
+                string result;
+
+                logicalMessage.Headers.TryGetValue(key, out result);
+
+                return result;
+            };
+            
+            //load saga
+            sagaPersistenceBehavior.Invoke(context, () => { });
+
+            var handlerInvocation = new InvokeHandlersBehavior();
+
+            handlerInvocation.Invoke(new HandlerInvocationContext(context,messageHandler),()=>{});
+
+
+            sagaActivated = context.TryGet(out sagaInstance);
+        }
+
+        public void AssertIsStarted()
+        {
+            Assert.True(sagaActivated && !sagaInstance.NotFound);
+        }
+
+        HandlerInvocationContext context;
+
+        ActiveSagaInstance sagaInstance;
+
+        FuncBuilder builder = new FuncBuilder();
+
+        bool sagaActivated;
+
+        public void AssertResultingMessages(Predicate<List<CapturedMessage>> predicate)
+        {
+            Assert.True(predicate(capturedMessages));
+        }
+
+        List<CapturedMessage> capturedMessages = new List<CapturedMessage>();
+    }
+
+    public class CapturedMessage
+    {
+        public bool OfType<T>()
+        {
+            return typeof(T) == metadata.MessageType;
+        }
+
+        MessageMetadata metadata;
+
+        public CapturedMessage(MessageMetadata metadata)
+        {
+            this.metadata = metadata;
+        }
+    }
+}
--- src/ServiceControl.UnitTests/AAA/Sample.cs
+++ src/ServiceControl.UnitTests/AAA/Sample.cs
@@ -1,39 +1,54 @@
 ﻿namespace ServiceControl.UnitTests.AAA
 {
     using System;
-    using System.Collections.Generic;
-    using NServiceBus.Pipeline.Contexts;
+    using System.Linq;
+    using NServiceBus;
     using NServiceBus.Saga;
-    using NServiceBus.Sagas;
-    using NServiceBus.Unicast.Behaviors;
-    using NServiceBus.Unicast.Messages;
     using NUnit.Framework;
 
     [TestFixture]
     public class Sample
     {
         [Test]
-        public void CheckStartMessage()
+        public void Should_start_saga()
         {
             var sagaToTest = Test.Saga<MySaga>();
 
-            sagaToTest.When(new StartedMessage());
+            sagaToTest.When(new StartMessage());
 
             sagaToTest.AssertIsStarted();
         }
-
-        class MySaga : Saga<MySagaData>
+        [Test]
+        public void Should_capture_bus_interactions()
         {
-            
+            var sagaToTest = Test.Saga<MySaga>();
+
+            sagaToTest.When(new StartMessage());
+
+            sagaToTest.AssertResultingMessages(messages => messages.Any(m=>m.OfType<StartMessage>()));
+        }
+
+        class MySaga : Saga<MySagaData>,IAmStartedByMessages<StartMessage>
+        {
+            public void Handle(StartMessage message)
+            {
+                Bus.Send(new MessageSentBySaga());
+            }
         }
 
         class MySagaData : ContainSagaData
         {
+            public Guid IdToCorrelateOn { get; set; }
         }
     }
 
-    public class StartedMessage
+    class MessageSentBySaga : IMessage
     {
+    }
+
+    public class StartMessage:IMessage
+    {
+        public Guid IdToCorrelateOn { get; set; }
     }
 
     public class Test
@@ -42,51 +57,5 @@
         {
             return new SagaSpecification<T>();
         }
-    }
-
-    public class SagaSpecification<T>
-    {
-     
-        public void Given(object message)
-        {
-            
-        }
-
-        public void When<TMessage>(TMessage message)
-        {
-            ApplyMessageToSaga(message);
-        }
-
-        void ApplyMessageToSaga(object message)
-        {
-            var metadata = new MessageMetadata();
-            metadata.MessageType = message.GetType();
-
-            var logicalMessage = new LogicalMessage(metadata, message, new Dictionary<string, string>());
-
-            context = new HandlerInvocationContext(new ReceiveLogicalMessageContext(new RootContext(builder), logicalMessage), new MessageHandler
-            {
-                Instance = Activator.CreateInstance<T>()
-            });
-
-            var sagaPersitenceBehavior = new SagaPersistenceBehavior();
-
-            sagaPersitenceBehavior.Invoke(context, () => { });
-
-            sagaActivated = context.TryGet(out sagaInstance);
-        }
-
-        public void AssertIsStarted()
-        {
-            Assert.True(sagaActivated && !sagaInstance.NotFound);
-        }
-
-        HandlerInvocationContext context;
-
-        ActiveSagaInstance sagaInstance;
-
-        FuncBuilder builder = new FuncBuilder();
-
-        bool sagaActivated;
     }
 }
